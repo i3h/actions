477b460484522689b37da77bb6b3af14217e6163
noobly <noobly314@gmail.com>
2019-11-16 14:38:38 +0800

    add action exec

---
+++ exec/README.md
@@ -0,0 +1,13 @@
+## rsync
+
+```
+- name: rsync to remote server
+  uses: noobly314/actions/rsync@master
+  env:
+    SERVER: YOUR_SERVER
+    USERNAME: YOUR_USERNAME
+    SRC: SOURCE_DIRECTORY
+    DEST: DESTINATION_DIRECTORY
+    KEY: PRIVATE_KEY
+
+```
---
+++ exec/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# set up ssh secrets
+SSH_PATH="$HOME/.ssh"
+mkdir "$SSH_PATH"
+echo "$KEY" > "$SSH_PATH/key"
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/key"
+
+# execute command on remote server
+sh -c "ssh -i $SSH_PATH/key -o StrictHostKeyChecking=no $USERNAME@$SERVER $*"
