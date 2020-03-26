a413712349f30fa75672858f8b9828bde852a9e1
noobly314 <noobly314@gmail.com>
2020-03-26 21:54:31 +0800

    test

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -3,11 +3,18 @@
 # set up ssh secrets
 SSH_PATH=$HOME/.ssh
 mkdir $SSH_PATH
-echo "$KEY" > $SSH_PATH/key
-echo "$PROXY_KEY" > $SSH_PATH/proxy_key
-chmod 700 $SSH_PATH
-chmod 600 $SSH_PATH/key
-chmod 600 $SSH_PATH/proxy_key
-
-# rsync to remote server
-rsync -v -a --progress --delete -e "ssh -o StrictHostKeyChecking=no -o ProxyCommand='ssh -o StrictHostKeyChecking=no -W %h:%p -i $SSH_PATH/proxy_key $PROXY_USERNAME@$PROXY_SERVER' -i $SSH_PATH/key" $SRC $USERNAME@$SERVER:$DEST
+if [ -z "$PROXY_KEY" ]; then
+        echo "$KEY" > $SSH_PATH/key
+        chmod 700 $SSH_PATH
+        chmod 600 $SSH_PATH/key
+        # rsync to remote server
+        rsync -v -a --progress --delete -e "ssh -o StrictHostKeyChecking=no -i $SSH_PATH/key" $SRC $USERNAME@$SERVER:$DEST
+else
+        echo "$KEY" > $SSH_PATH/key
+        echo "$PROXY_KEY" > $SSH_PATH/proxy_key
+        chmod 700 $SSH_PATH
+        chmod 600 $SSH_PATH/key
+        chmod 600 $SSH_PATH/proxy_key
+        # rsync to remote server
+        rsync -v -a --progress --delete -e "ssh -o StrictHostKeyChecking=no -o ProxyCommand='ssh -o StrictHostKeyChecking=no -W %h:%p -i $SSH_PATH/proxy_key $PROXY_USERNAME@$PROXY_SERVER' -i $SSH_PATH/key" $SRC $USERNAME@$SERVER:$DEST
+fi
