59416f30de711f7ef030046ca60e1bb8465369e2
noobly314 <noobly314@gmail.com>
2020-03-26 13:46:02 +0800

    test

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -6,7 +6,8 @@ mkdir $SSH_PATH
 echo $KEY > $SSH_PATH/key
 echo $PROXY_KEY > $SSH_PATH/proxy_key
 chmod 700 $SSH_PATH
-chmod 600 $SSH_PATH/*
+chmod 600 $SSH_PATH/key
+chmod 600 $SSH_PATH/proxy_key
 
 # rsync to remote server
-rsync -v -a --progress --delete -e "ssh -o StrictHostKeyChecking=no -o ProxyCommand='ssh -o StrictHostKeyChecking=no -W %h:%p -i $SSH_PATH/proxy_key ${PROXY_USERNAME}@${PROXY_SERVER}' -i ${SSH_PATH}/key" $SRC $USERNAME@$SERVER:$DEST
+sh -c `rsync -v -a --progress --delete -e "ssh -o StrictHostKeyChecking=no -o ProxyCommand='ssh -o StrictHostKeyChecking=no -W %h:%p -i $SSH_PATH/proxy_key ${PROXY_USERNAME}@${PROXY_SERVER}' -i ${SSH_PATH}/key" $SRC $USERNAME@$SERVER:$DEST`
