85a48a8161fad484ee8cd640691bf4735f9afcaa
noobly314 <noobly314@gmail.com>
2020-03-27 15:58:25 +0800

    test

--- dl-assets/entrypoint.sh
+++ dl-assets/entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-wget -O $FILE https://github.com/$REPOSITORY/releases/$VERSION/download/$FILE
+if [ "$VERSION" == "latest" ]
+	URL=https://github.com/$REPOSITORY/releases/latest/download/$FILE
+else
+	URL=https://github.com/$REPOSITORY/releases/download/$VERSION/$FILE
+fi
+wget -O $FILE $URL
